d35f3200a1d4e3141e5ce1965a9840c39c486e32
marjauzann <marjauzann@gmail.com>
2024-03-23 08:55:37 +0700

    menambahkan class SimpleDataBase

--- modul5_1302220009/Program.cs
+++ modul5_1302220009/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul5_1302220009;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<long>data = new SimpleDataBase <long>();
+        data.AddNewData(13);
+        data.AddNewData(02);
+        data.AddNewData(22);
+        data.PrintAllData();
+    }
+}
---
+++ modul5_1302220009/SimpleDataBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302220009
+{
+    internal class SimpleDataBase<T>
+    {
+        private List <T> storedData;
+        private List <DateTime> inputDates;
+        
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T newData)
+        {
+            storedData.Add(newData);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i+1} berisi: {storedData.ElementAt(i)}" + $"yang disimpan pada waktu {inputDates.ElementAt(i)}");
+            } 
+        }
+    }
+}
